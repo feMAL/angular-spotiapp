85725cd534c4f86f3d4d6214a7be75a747c6eaf6
Fernando Nahuel <falgorm@gmail.com>
2020-06-23 20:33:54 -0300

    Update service.ts

--- src/app/services/conf/service.ts
+++ src/app/services/conf/service.ts
@@ -4,6 +4,6 @@ export const config = {
         newReleases: 'browse/new-releases',
         albumsByArtist: 'search'
     },
-    access_token: "Bearer BQBXCBFh8IOhj4w1lk1fCzLHe9NpQfHtjG1zbP2Y9xAR_SjpL6k_CsGfKWYPLd2nqOfqF7jQbJxXkz22PuREg_oXZ-AsVldyepqAIItnuJxZctSZSXyFAFa-SxY2M24h0m0U3HNy-1-wI70MTCwSo5raND_hjvAK97T8hz4gp4KBVFxyyTgIwlU"
+    access_token: "Bearer {{TOKEN_SPOTIFY}}"
     
 }
